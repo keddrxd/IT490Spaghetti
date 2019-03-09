5e0963dfaced5e4c410486a03b9516b2d11371b6
Aalap Parekh <ap732@njit.edu>
2019-03-09 12:20:05 -0500

    Update successPage.php
    design

--- successPage.php
+++ successPage.php
@@ -1,13 +1,38 @@
+
+
 <?php
 session_start();
 require_once $_SERVER['DOCUMENT_ROOT'].'/checkSession.php';
 ?>
+
+<!DOCTYPE html>
 <html>
+<body background = "spaghetti.jpg">
+<head>
+<center>
 <h1>Login Page</h1>
-<body>
-<div id="textResponse">
-Successfully logged in!
-<a href="logout.php"><button>LOGOUT</button></a>
+<center>
+<style>
+div {
+    background-color: white;
+    width: 300px;
+    border: 5px solid black;	    
+    padding: 25px;
+    margin: 25px;
+}
+
+</style>
+</head>
+<center>
+		<div id = "textResponse">
+		<h2> Successfully Logged In </h2> 
+		<br>
+		<a href="logout.php"><button>Sign Out</button></a>
+		</center>
+		</div>
+</center>
+
+
 </div>
 </body>
 </html>
