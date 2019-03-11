b8c6f08fe7037a47692f80a8d44b4a56fd89de4d
andrew8197 <47332089+andrew8197@users.noreply.github.com>
2019-03-11 11:42:21 -0400

    Update server.php

--- server.php
+++ server.php
@@ -66,9 +66,7 @@ function auth($userN, $session)
 	$reply = $mysqli->query($query);
 	while($row = $reply->fetch_assoc())
 	{
-		echo $row['username'].PHP_EOL;
-		echo $row['sessionKey'].PHP_EOL;
-		if($row['sessionKey'] == $session)
+		if($row["sessionKey"] == $session)
 		{
 			echo "Session ID's match!".PHP_EOL;
 			$userData['username'] = $userN;
