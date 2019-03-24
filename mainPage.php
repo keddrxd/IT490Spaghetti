6a989e3ed157de2feb5335ab635b4448e6136db0
andrew8197 <47332089+andrew8197@users.noreply.github.com>
2019-03-24 17:14:22 -0400

    Update mainPage.php

--- mainPage.php
+++ mainPage.php
@@ -102,10 +102,13 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST')
 		</marquee>
 		</center>
 		</div>
+	
+	<form action="index.php" >
 
 		<div id = "logout">
-		<button name = "logout" style = "height:50px;width:80px" formaction = "index.php"><font size = 4> Logout </font></button>
+		<button name = "logout" style = "height:50px;width:80px"><font size = 4> Logout </font></button>
 		</div>
+	</form>
 		
 		<div id = "body">
 		<p>Hello testing</p>
