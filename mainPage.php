bc3d2282940fec861440f3358d879ed239791358
andrew8197 <47332089+andrew8197@users.noreply.github.com>
2019-03-24 19:18:43 -0400

    Update mainPage.php

--- mainPage.php
+++ mainPage.php
@@ -22,18 +22,7 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST')
 		
 	}
 }
-	$host = '25.80.231.148';
-	$user = 'admin';
-	$pw = 'adminPwd';
-	$db = 'usersDB';
-	$mysqli = new mysqli($host, $user, $pw, $db);
-	$query = "select * from users where username = 'andypoo'";
-	$reply = $mysqli->query($query);
-	while ($row = $reply->fetch_assoc())
-	{
-		echo "<br> Your username is: ". row['username'];
-		echo "<br> Comedy = ". row['comedy'];
-	}
+	
 
 ?>
 
