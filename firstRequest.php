cfbe0c3e4c3b38e7ad4753dcf663d693ca1dc977
andrew8197 <47332089+andrew8197@users.noreply.github.com>
2019-03-24 18:33:31 -0400

    Update firstRequest.php

--- firstRequest.php
+++ firstRequest.php
@@ -1,8 +1,8 @@
 <?php
 
 require ('rabbitFunc.php');
-//$username = $_POST['username'];
-$username = "0";
+$username = $_POST['username'];
+//$username = "0";
 //$username = $_GET['username'];
 //$username = $_SESSION['username'];
 //$username = $_REQUEST['username'];
