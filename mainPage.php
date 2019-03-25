b04b193af5e58a15db3627ce4b499661184107fe
andrew8197 <47332089+andrew8197@users.noreply.github.com>
2019-03-24 23:16:42 -0400

    Update mainPage.php

--- mainPage.php
+++ mainPage.php
@@ -14,36 +14,40 @@
           echo "Failed to connect to MySQL: " . mysqli_connect_error();
           exit();
   	}
-	$s = mysqli_query($mysqli, "SELECT * FROM category WHERE username = 'andypoo'");
-	while ($row = mysqli_fetch_array($s, MYSQLI_ASSOC)) {
-		$user = $row["username"];
-		$comedy = $row["comedy"];
-		$horror = $row["horror"];
-		$action = $row["action"];
-		$scifi = $row["scifi"];
-		$romance = $row["romance"];
-		$animation = $row["animation"];
-	}
-	echo "<br>Hello $user !<br>";
-	echo "Your favorite categories are: <br>";
-	if($comedy !== '0'){
-		echo "$comedy<br>";	
-	}
-	if($horror !== '0'){
-		echo "$horror<br>";	
-	}
-	if($action !== '0'){
-		echo "$action<br>";	
-	}
-	if($scifi !== '0'){
-		echo "$scifi<br>";	
-	}
-	if($romance !== '0'){
-		echo "$romance<br>";	
-	}
-	if($animation !== '0'){
-		echo "$animation<br>";	
-	}
+		//$s = mysqli_query($mysqli, "SELECT * FROM category WHERE username = $user");
+		//$s = mysqli_query($mysqli, "Select * from category where category.username = '$username'");
+		$s = mysqli_query($mysqli, "select username from category where username ='".$_SESSION['username']."'");
+
+		while ($row = mysqli_fetch_array($s, MYSQLI_ASSOC)) {
+			$user = $row["username"];
+			$comedy = $row["comedy"];
+			$horror = $row["horror"];
+			$action = $row["action"];
+			$scifi = $row["scifi"];
+			$romance = $row["romance"];
+			$animation = $row["animation"];
+		}
+		echo "<br>Hello $user !<br>";
+		echo "Your favorite categories are: <br>";
+		if($comedy !== '0'){
+			echo "$comedy<br>";	
+		}
+		if($horror !== '0'){
+			echo "$horror<br>";	
+		}
+		if($action !== '0'){
+			echo "$action<br>";	
+		}
+		if($scifi !== '0'){
+			echo "$scifi<br>";	
+		}
+		if($romance !== '0'){
+			echo "$romance<br>";	
+		}
+		if($animation !== '0'){
+			echo "$animation<br>";	
+		}
+	
 ?>
 	
 <?php
