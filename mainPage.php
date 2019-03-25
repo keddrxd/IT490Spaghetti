9fe42dc4ca18db65120bf8ac1e88c476b62cd281
andrew8197 <47332089+andrew8197@users.noreply.github.com>
2019-03-24 22:34:24 -0400

    Update mainPage.php

--- mainPage.php
+++ mainPage.php
@@ -25,7 +25,7 @@
 		$animation = $row["animation"];
 	}
 	echo "<br><br><br><br><br><br><br><br><br>Hello $user !<br>";
-	echo "Your favorite categories are: <br>"
+	echo "Your favorite categories are: <br>";
 	if($comedy != 0){
 		echo "$comedy<br>";	
 	}
