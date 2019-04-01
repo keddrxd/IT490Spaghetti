ef44ae4b9f4bfd2ba978b30984ac30363bcc1661
andrew8197 <47332089+andrew8197@users.noreply.github.com>
2019-04-01 17:35:11 -0400

    Update test.py

--- test.py
+++ test.py
@@ -53,45 +53,62 @@ for i in rsp_json["results"]:
     horror = 27
     romance = 10749
     scifi = 878
+    mycursor = mydb.cursor()
+    sql1 = "INSERT INTO comedy (comedy, rd) VALUES (%s,%s)"
+    sql2 = "INSERT INTO horror (horror, rd) VALUES (%s,%s)"
+    sql3 = "INSERT INTO action (action, rd) VALUES (%s,%s)"
+    sql4 = "INSERT INTO scifi (scifi, rd) VALUES (%s,%s)"
+    sql5 = "INSERT INTO romance (romance, rd) VALUES (%s,%s)"
+    sql6 = "INSERT INTO animation (animation, rd) VALUES (%s,%s)"
     
     #print("genre: {}".format(i["genre_ids"]))
     for g in i["genre_ids"]:
         if comedy == g:
             comedyRec = i["title"]
             rd1 = i["release_date"]
+            val1 = (comedyRec, rd1)
+            mycursor.execute(sql1, val1)
+
             print(i["title"]+" - is a comedy movie. It will be released on "+i["release_date"])
         if horror == g:
             horrorRec = i["title"]
             rd2 = i["release_date"]
+            val2 = (horrorRec, rd2)
+            mycursor.execute(sql2, val2)
             print(i["title"]+" - is a horror movie. It will be released on "+i["release_date"])
         if action == g:
             #i["title"] = actionRec
             actionRec =  i["title"]
             rd3 = i["release_date"]
+            val3 = (actionRec, rd3)
+            mycursor.execute(sql3, val3)
             #print (actionRec+rd1)
             #print (rd1)
             print(i["title"]+" - is a action movie. It will be released on "+i["release_date"])
         if scifi == g:
             scifiRec = i["title"]
             rd4 = i["release_date"]
+            val4 = (scifiRec, rd4)
+            mycursor.execute(sql4, val4)
             print(i["title"]+" - is a science fiction movie. It will be released on "+i["release_date"])
         if romance == g:
             romanceRec = i["title"]
             rd5 = i["release_date"]
+            val5 = (romanceRec, rd5)
+            mycursor.execute(sql5, val5)
             print(i["title"]+" - is a romance movie. It will be released on "+i["release_date"])
         if animation == g:
             animationRec = i["title"]
             rd6 = i["release_date"]
+            val6 = (animationRec, rd6)
+            mycursor.execute(sql6, val6)
             print(i["title"]+" - is a animation movie. It will be released on "+i["release_date"])
         
         
-        
+    mydb.commit()
+
 
 #print(data.decode("utf-8"))
 
-mycursor = mydb.cursor()
-sql = "INSERT INTO movieRec (username, comedy, rd1, horror, rd2, action, rd3, scifi, rd4, romance, rd5, animation, rd6) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-val = (username, comedyRec, rd1, horrorRec, rd2, actionRec, rd3, scifiRec, rd4, romanceRec, rd5, animationRec, rd6)
-mycursor.execute(sql, val)
-mydb.commit()
+
                 
