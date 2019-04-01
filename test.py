c1a1d82d69b28c14e917cd342f57bafbfaa0f397
Adam Kedra <47116358+keddrxd@users.noreply.github.com>
2019-04-01 11:24:06 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,68 @@
+import http.client
+import json
+#import mysql.connector
+
+#mydb = mysql.connector.connect(
+#    host = "localhost",
+#    user = "admin",
+#    password = "adminPwd",
+#    database = "usersDB"
+#)
+
+#mycursor = mydb.cursor()
+
+
+
+
+
+
+conn = http.client.HTTPSConnection("api.themoviedb.org")
+
+payload = "{}"
+
+
+conn.request("GET", "/3/movie/upcoming?region=US&page=1&language=en-US&api_key=f8b954579281e43ef2cd96c216d57cb2", payload)
+
+res = conn.getresponse()
+data = res.read()
+
+#convert to json
+rsp_json = json.loads(data.decode("utf-8"))
+#print(rsp_json)
+
+
+
+for i in rsp_json["results"]:
+
+    actionRec = ""
+
+    action = 28
+    
+    comedy = 35
+    animation = 16
+    horror = 27
+    romance = 10749
+    scifi = 878
+    #print("genre: {}".format(i["genre_ids"]))
+    for g in i["genre_ids"]:
+        if action == g:
+            i["title"] = actionRec
+            #i['release_date'] = rd1
+            print (actionRec)
+            print(i["title"]+" - is a action movie. It will be released on "+i["release_date"])
+        if comedy == g:
+            print(i["title"]+" - is a comedy movie. It will be released on "+i["release_date"])
+        if animation == g:
+            print(i["title"]+" - is a animation movie. It will be released on "+i["release_date"])
+        if horror == g:
+            print(i["title"]+" - is a horror movie. It will be released on "+i["release_date"])
+        if romance == g:
+            print(i["title"]+" - is a romance movie. It will be released on "+i["release_date"])
+        if scifi == g:
+            print(i["title"]+" - is a science fiction movie. It will be released on "+i["release_date"])
+
+#print(data.decode("utf-8"))
+
+
+#sql = "INSERT INTO movieRec (movieName)
+            
