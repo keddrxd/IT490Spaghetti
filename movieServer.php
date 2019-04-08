48203467728020d38607c72940abce991da4169a
andrew8197 <47332089+andrew8197@users.noreply.github.com>
2019-04-08 18:07:56 -0400

    Update movieServer.php

--- movieServer.php
+++ movieServer.php
@@ -17,6 +17,62 @@ if (mysqli_connect_errno())
 }
 
 
+function getMovies($comedy, $rd1, $horror, $rd2, $action, $rd3, $scifi, $rd4, $romance, $rd5, $animation, $rd6)
+{
+	$host = '127.0.0.1';
+	$user = 'admin';
+	$pw = 'adminPwd';
+	$db = 'usersDB';
+	$mysqli = new mysqli($host, $user, $pw, $db); 
+	if (mysqli_connect_errno($mysqli))
+  	{
+          echo "Failed to connect to MySQL: " . mysqli_connect_error();
+          exit();
+  	}
+	
+	$userData = array();
+	$comedy = $mysqli->escape_string($comedy);
+	$rd1 = $mysqli->escape_string($rd1);
+	$horror = $mysqli->escape_string($horror);
+	$rd2 = $mysqli->escape_string($rd2);
+	$action = $mysqli->escape_string($action);
+	$rd3 = $mysqli->escape_string($rd3);
+	$scifi = $mysqli->escape_string($scifi);
+	$rd4 = $mysqli->escape_string($rd4);
+	$romance = $mysqli->escape_string($romance);
+	$rd5 = $mysqli->escape_string($rd5);
+	$animation = $mysqli->escape_string($animation);
+	$rd6 = $mysqli->escape_string($rd6);
+	
+	$query1 = "INSERT INTO comedy ($comedy, $rd1)";
+	//$reply1 = $mysqli->query($query1);
+	$mysqli->query($query1) or die($mysqli->error);
+	
+	$query2 = "INSERT INTO horror ($horror, $rd2)";
+	//$reply1 = $mysqli->query($query1);
+	$mysqli->query($query2) or die($mysqli->error);
+	
+	$query3 = "INSERT INTO action ($action, $rd3)";
+	//$reply1 = $mysqli->query($query1);
+	$mysqli->query($query3) or die($mysqli->error);
+	
+	$query4 = "INSERT INTO scifi ($scifi, $rd4)";
+	//$reply1 = $mysqli->query($query1);
+	$mysqli->query($query4) or die($mysqli->error);
+	
+	$query5 = "INSERT INTO romance ($romance, $rd5)";
+	//$reply1 = $mysqli->query($query1);
+	$mysqli->query($query5) or die($mysqli->error);
+	
+	$query6 = "INSERT INTO animation ($animation, $rd6)";
+	//$reply1 = $mysqli->query($query1);
+	$mysqli->query($query6) or die($mysqli->error);
+
+	
+	
+	
+}
+
 
 
 $server = new rabbitMQServer("testRabbitMQ.ini","testServer");
