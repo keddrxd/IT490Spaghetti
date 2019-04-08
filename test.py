1d8b2550dc30866ba6f98ee441ea716e37fb13f1
Adam Kedra <47116358+keddrxd@users.noreply.github.com>
2019-04-08 17:19:07 -0400

    Update test.py

--- test.py
+++ test.py
@@ -22,9 +22,7 @@ mydb = mysql.connector.connect(
 
 
 
-
-
-
+credentials = pika.PlainCredentials(RABBIT_HOST,RABBIT_PASS)
 
 
 conn = http.client.HTTPSConnection("api.themoviedb.org")
@@ -42,21 +40,30 @@ rsp_json = json.loads(data.decode("utf-8"))
 #print(rsp_json)
 
 
+def sendMovies(rabbitMQServer, rabbitQueue, rabbitUser, rabbitPass, rabbitVhost, rabbitEX, rabbitPort, movieTitle, releaseDates, genre):
+
+    movieDict = {
+        'title':movieTitle,
+        'releasedates':releaseDates,
+        'genre': genre
+
+    }
+
+    rabbitMQMessage = json.dumps(movieDict , sort_keys=true, ident = 4, default=str)
+
+    try: 
+        connection = pika.BlockingConnection(pika.ConnectionParameters(rabbitMQServer, rabbitPort, rabbitVhost, credentials))
+        channel = connection.channel()
+        channel.basic_publish(exchange=rabbitEX, routing_key=rabbitQueue,body=rabbitMQMessage)
+
+
+
+
+
 
 for i in rsp_json["results"]:
     username = "andypoo"
-    #comedyRec = ""
-   # rd1 = ""
-   # horrorRec = ""
-   # rd2 = ""
-    #actionRec = ""
-    #rd3 = ""
-   # scifiRec = ""
-   # rd4 = ""
-    #romanceRec = ""
-    #rd5 = ""
-   # animationRec = ""
-   # rd6 = ""
+  
     
     action = 28    
     comedy = 35
@@ -79,14 +86,18 @@ for i in rsp_json["results"]:
             rd1 = i["release_date"]
             val1 = (comedyRec, rd1)
             mycursor.execute(sql1, val1)
+            sendMovies(RABBIT_HOST,RABBIT_Q,RABBIT_USER,RABBIT_PASS,RABBIT_VH,RABBIT_EX,RABBIT_PORT,comedyRec,rd1,"Comedy")
 
             print(i["title"]+" - is a comedy movie. It will be released on "+i["release_date"])
+
         if horror == g:
             horrorRec = i["title"]
             rd2 = i["release_date"]
             val2 = (horrorRec, rd2)
             mycursor.execute(sql2, val2)
+            sendMovies(RABBIT_HOST,RABBIT_Q,RABBIT_USER,RABBIT_PASS,RABBIT_VH,RABBIT_EX,RABBIT_PORT,horrorRec,rd2,"Horror")
             print(i["title"]+" - is a horror movie. It will be released on "+i["release_date"])
+
         if action == g:
             #i["title"] = actionRec
             actionRec =  i["title"]
@@ -95,31 +106,47 @@ for i in rsp_json["results"]:
             mycursor.execute(sql3, val3)
             #print (actionRec+rd1)
             #print (rd1)
+            sendMovies(RABBIT_HOST,RABBIT_Q,RABBIT_USER,RABBIT_PASS,RABBIT_VH,RABBIT_EX,RABBIT_PORT,actionRec,rd3,"Action")
             print(i["title"]+" - is a action movie. It will be released on "+i["release_date"])
+
         if scifi == g:
             scifiRec = i["title"]
             rd4 = i["release_date"]
             val4 = (scifiRec, rd4)
             mycursor.execute(sql4, val4)
+            sendMovies(RABBIT_HOST,RABBIT_Q,RABBIT_USER,RABBIT_PASS,RABBIT_VH,RABBIT_EX,RABBIT_PORT,scifi,rd4,"Scifi")
             print(i["title"]+" - is a science fiction movie. It will be released on "+i["release_date"])
+        
         if romance == g:
             romanceRec = i["title"]
             rd5 = i["release_date"]
             val5 = (romanceRec, rd5)
             mycursor.execute(sql5, val5)
+            sendMovies(RABBIT_HOST,RABBIT_Q,RABBIT_USER,RABBIT_PASS,RABBIT_VH,RABBIT_EX,RABBIT_PORT,romanceRec,rd5,"Romance")
             print(i["title"]+" - is a romance movie. It will be released on "+i["release_date"])
+        
         if animation == g:
             animationRec = i["title"]
             rd6 = i["release_date"]
             val6 = (animationRec, rd6)
             mycursor.execute(sql6, val6)
+            sendMovies(RABBIT_HOST,RABBIT_Q,RABBIT_USER,RABBIT_PASS,RABBIT_VH,RABBIT_EX,RABBIT_PORT,animationRec,rd6,"Animation")
             print(i["title"]+" - is a animation movie. It will be released on "+i["release_date"])
         
         
     mydb.commit()
 
 
-print(data.decode("utf-8"))
+
+
+
+
+
+
+
+
+
+#print(data.decode("utf-8"))
 
 
 
