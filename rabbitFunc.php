b3370050153b653e93df36a208751cc7687f5699
andrew8197 <47332089+andrew8197@users.noreply.github.com>
2019-04-08 18:15:51 -0400

    Update rabbitFunc.php

--- rabbitFunc.php
+++ rabbitFunc.php
@@ -57,6 +57,28 @@ function firstLogin($username, $comedy, $horror, $action, $scifi, $romance, $ani
 
 }
 
+function requestMovies($comedy, $rd1, $horror, $rd2, $action, $rd3, $scifi, $rd4, $romance, $rd5, $animation, $rd6)
+{
+	$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+	$request6= array();	
+	$request6['type']="requestMovies";
+	$request6['comedy']= $comedy;
+	$request6['rd1']= $rd1;
+	$request6['horror']= $horror;
+	$request6['rd2']= $rd2;
+	$request6['action']= $action;
+	$request6['rd3']= $rd3;
+	$request6['scifi']= $scifi;
+	$request6['rd4']= $rd4;
+	$request6['romance']= $romance;
+	$request6['rd5']= $rd5;
+	$request6['animation']= $animation;
+	$request6['rd6']= $rd6;
+	$response= $client->send_request($request5);
+	return $response;	
+	
+}
+
 
 
 
