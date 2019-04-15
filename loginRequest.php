b3f95d35bd1c8c8e417c6ec34fd549b87b286ba9
andrew8197 <47332089+andrew8197@users.noreply.github.com>
2019-04-15 18:14:08 -0400

    Update loginRequest.php

--- loginRequest.php
+++ loginRequest.php
@@ -4,6 +4,7 @@ require ('rabbitFunc.php');
 $user = $_POST['username'];
 $pass = $_POST['password'];
 $response = login($user,$pass);
+$response1 = userRecc($user);
 if( $response != false)
 {
 	$sessionData = json_decode($response, true);
@@ -13,7 +14,13 @@ if( $response != false)
 	$_SESSION['firstName'] = $sessionData['firstName'];
 	$_SESSION['lastName'] = $sessionData['lastName'];
 	$_SESSION['zip'] = $sessionData['zip'];
-	
+	$sessionData1 = json_decode($response1, true);
+	$_SESSION['comedy'] = $sessionData1['comedy'];
+	$_SESSION['horror'] = $sessionData1['horror'];
+	$_SESSION['action'] = $sessionData1['action'];
+	$_SESSION['scifi'] = $sessionData1['scifi'];
+	$_SESSION['romance'] = $sessionData1['romance'];
+	$_SESSION['animation'] = $sessionData1['animation'];
 	header("location: mainPage.php");
 }
 else
