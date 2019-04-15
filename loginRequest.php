9fc2e915c40c1ba444f244f8eefc12269824d2c4
andrew8197 <47332089+andrew8197@users.noreply.github.com>
2019-04-15 18:23:07 -0400

    Update loginRequest.php

--- loginRequest.php
+++ loginRequest.php
@@ -4,7 +4,13 @@ require ('rabbitFunc.php');
 $user = $_POST['username'];
 $pass = $_POST['password'];
 $response = login($user,$pass);
-$response1 = userRecc($user);
+$response11 = userRecc($user);
+$response1 = comRecc($user);
+$response2 = horRecc($user);
+$response3 = actRecc($user);
+$response4 = sciRecc($user);
+$response5 = romRecc($user);
+$response6 = aniRecc($user);
 if( $response != false)
 {
 	$sessionData = json_decode($response, true);
@@ -14,13 +20,62 @@ if( $response != false)
 	$_SESSION['firstName'] = $sessionData['firstName'];
 	$_SESSION['lastName'] = $sessionData['lastName'];
 	$_SESSION['zip'] = $sessionData['zip'];
+	$sessionData11 = json_decode($response11, true);
+	$_SESSION['comedy'] = $sessionData11['comedy'];
+	$_SESSION['horror'] = $sessionData11['horror'];
+	$_SESSION['action'] = $sessionData11['action'];
+	$_SESSION['scifi'] = $sessionData11['scifi'];
+	$_SESSION['romance'] = $sessionData11['romance'];
+	$_SESSION['animation'] = $sessionData11['animation'];
+	
 	$sessionData1 = json_decode($response1, true);
-	$_SESSION['comedy'] = $sessionData1['comedy'];
-	$_SESSION['horror'] = $sessionData1['horror'];
-	$_SESSION['action'] = $sessionData1['action'];
-	$_SESSION['scifi'] = $sessionData1['scifi'];
-	$_SESSION['romance'] = $sessionData1['romance'];
-	$_SESSION['animation'] = $sessionData1['animation'];
+	$_SESSION['comedyArray'] = array();
+	$num = 3;
+	for($i = 0 ; $i < $num ; $i++)
+	{
+		$_SESSION['comedyArray'][$i] = $sessionData1[$i];
+	}
+	
+	$sessionData2 = json_decode($response2, true);
+	$_SESSION['horrorArray'] = array();
+	for($i = 0 ; $i < $num ; $i++)
+	{
+		$_SESSION['horrorArray'][$i] = $sessionData2[$i];
+	}
+	
+	$sessionData3 = json_decode($response3, true);
+	$_SESSION['actionArray'] = array();
+	for($i = 0 ; $i < $num ; $i++)
+	{
+		$_SESSION['actionArray'][$i] = $sessionData3[$i];
+	}
+	
+	$sessionData4 = json_decode($response4, true);
+	$_SESSION['scifiArray'] = array();
+	for($i = 0 ; $i < $num ; $i++)
+	{
+		$_SESSION['scifiArray'][$i] = $sessionData4[$i];
+	}
+	
+	$sessionData5 = json_decode($response5, true);
+	$_SESSION['romanceArray'] = array();
+	for($i = 0 ; $i < $num ; $i++)
+	{
+		$_SESSION['romanceArray'][$i] = $sessionData5[$i];
+	}
+	
+	$sessionData6 = json_decode($response6, true);
+	$_SESSION['animationArray'] = array();
+	for($i = 0 ; $i < $num ; $i++)
+	{
+		$_SESSION['animationArray'][$i] = $sessionData6[$i];
+	}
+	
+	
+	
+	
+	
+	
 	header("location: mainPage.php");
 }
 else
