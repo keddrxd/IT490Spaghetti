9eefbb1a5563e495ab5e00af0c851c6d5d0e5dd0
andrew8197 <47332089+andrew8197@users.noreply.github.com>
2019-04-15 16:36:31 -0400

    Update mainPage.php

--- mainPage.php
+++ mainPage.php
@@ -18,11 +18,12 @@ $romance = $_SESSION['romance'];
 $animation = $_SESSION['animation'];
 	
 echo "Hello ".$_SESSION['username'];
+echo "<br>";
 $num = 3;
 	
 if($comedy !== "")
 {
-	echo "<br> Here are some comedy movies you may like:<br>";
+	echo "<br><br> Here are some comedy movies you may like:<br>";
 	for($i = 0 ; $i < count($_SESSION['comedyArray']) ; $i++)
 	{
 		echo "<br>".$_SESSION['comedyArray'][$i];	
@@ -31,7 +32,7 @@ if($comedy !== "")
 
 if($horror !== "")
 {
-	echo "<br> Here are some horror movies you may like:<br>";
+	echo "<br><br> Here are some horror movies you may like:<br>";
 	for($i = 0 ; $i < count($_SESSION['horrorArray']) ; $i++)
 	{
 		echo "<br>".$_SESSION['horrorArray'][$i];	
@@ -40,7 +41,7 @@ if($horror !== "")
 
 if($action !== "")
 {
-	echo "Here are some action movies you may like:<br>";
+	echo "<br>Here are some action movies you may like:<br>";
 	for($i = 0 ; $i < count($_SESSION['actionArray']) ; $i++)
 	{
 		echo "<br>".$_SESSION['actionArray'][$i];	
@@ -49,7 +50,7 @@ if($action !== "")
 	
 if($scifi !== "")
 {
-	echo "Here are some sci-fi movies you may like:<br>";
+	echo "<br>Here are some sci-fi movies you may like:<br>";
 	for($i = 0 ; $i < count($_SESSION['scifiArray']) ; $i++)
 	{
 		echo "<br>".$_SESSION['scifiArray'][$i];	
@@ -58,7 +59,7 @@ if($scifi !== "")
 	
 if($romance !== "")
 {
-	echo "Here are some romance movies that you may like:<br>";
+	echo "<br>Here are some romance movies that you may like:<br>";
 	for($i = 0 ; $i < count($_SESSION['romanceArray']) ; $i++)
 	{
 		echo "<br>".$_SESSION['romanceArray'][$i];	
@@ -67,7 +68,7 @@ if($romance !== "")
 	
 if($animation !== "")
 {
-	echo "Here are some animation movies you may like:<br>";
+	echo "<br>Here are some animation movies you may like:<br>";
 	for($i = 0 ; $i < count($_SESSION['animationArray']) ; $i++)
 	{
 		echo "<br>".$_SESSION['animationArray'][$i];	
