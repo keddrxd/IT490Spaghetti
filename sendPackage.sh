641a226af853945e7c839f41e2aa452cafa5cad0
Aalap Parekh <ap732@njit.edu>
2019-04-16 18:55:31 -0400

    sendPackage.sh
    working on packaging

---
+++ sendPackage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+serverType = $1
+versionNum = $2
+
+echo "Creating package for "$serverType" version #: "$versionNum
+
+cd /home/$USER/git/
+
+tar -cf $serverType '_'$versionNum.tar --exclude-vcs IT490Spaghetti
+
+echo "Package "$serverType'_'$versionNum was created"
+
+sshpass -p "password" scp $serverType'_'$versionNum.tar adam@25.73.116.140:/home/packages/
