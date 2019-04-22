d34210362ab83bd7d7d93e803373884c7e31f88f
andrew8197 <47332089+andrew8197@users.noreply.github.com>
2019-04-22 19:43:09 -0400

    Update loginRequest.php

--- backend/loginRequest.php
+++ backend/loginRequest.php
@@ -11,6 +11,12 @@ $response3 = actRecc($user);
 $response4 = sciRecc($user);
 $response5 = romRecc($user);
 $response6 = aniRecc($user);
+$response13 = comRd($user);
+$response8 = horRd($user);
+$response9 = actRd($user);
+$response10 = sciRd($user);
+$response11 = romRd($user);
+$response12 = aniRd($user);
 if( $response != false)
 {
 	$sessionData = json_decode($response, true);
