40f0bcd56c63724e2b02e66e412e84133266a344
Adam Kedra <47116358+keddrxd@users.noreply.github.com>
2019-04-22 18:08:59 -0400

    Add files via upload

---
+++ deployment/createPackage.php
@@ -0,0 +1,42 @@
+#!/usr/bin/php 
+<?
+
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+global $argv;
+
+function versionCheck($server)
+{
+		echo "Checking the package version for".$server.PHP_EOL;
+		$client = new rabbitMQClient("testRabbitMQ.ini","deploymentServer");
+		request = array();
+		request['type'] = "versionCheck";
+		request['serverType'] = $server;
+		$response = $client->send_request($request);
+		$sessionData = json_decode($response,true);
+		$currentVersion = $sessionData['versionNumber'];
+		$futureVersion = $versionNumber+1;
+		echo "New version for ".$server." is ".$futureVersion.PHP_EOL;
+		return $futureVersion;
+}
+
+function createNewVersion($server)
+{
+		$client = new rabbitMQClient("testRabbitMQ.ini","deploymentServer");
+		$futureVersion = versionCheck($server);
+		bash(`sendPackage.sh`);
+		$request = array();
+		$request['type'] = "newPackage";
+		$request['serverType'] = $server;
+		$request['versionNumber'] = $futureVersion;
+		$request['packageName'] = $machine.'_'.$futureVersion.'.tar';
+		$client->send_request($request);
+}
+
+$serverType = $argv[1];
+
+echo "Creating new version: ".$serverType.PHP_EOL;
+createNewVersion($serverType);
+echo "New version created successfully.".PHP_EOL;
---
+++ deployment/deploymentServer.php
@@ -0,0 +1,73 @@
+#!/usr/bin/php
+
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+function versionCheck($serverType)
+{
+		#connection to Database
+		$host = 'localhost';
+		$user = 'adam';
+		$pass = 'deployPass';
+		$db = 'versionControl';
+		$mysqli = new mysqli($host,$user,$pass,$db);
+		if ($mysqli->connect_error)
+		{
+			die('Connect Error');
+		}
+		$packageData = array();
+		$query = "select verName from version where serverType= '$serverType' order by verName desc limit 1";
+		$versionNumber = $mysqli->query($query);
+		$versionNumber = $versionNumber->fetch_assoc();
+		echo "Current Version Number is ".$versionNumber['verName'];
+		$packageData['versionNumber'] = versionNumber['verName'];
+		return json_encode($packageData);
+}
+
+function addVersion($serverType,$versionNumber,$packageName)
+{
+	
+	$host = 'localhost';
+	$user = 'adam';
+	$pass = 'deployPass';
+	$db = 'versionControl';
+	$mysqli = new mysqli($host,$user,$pass,$db);
+	if ($mysqli->connect_error)
+		{
+			die('Connect Error');
+		}
+	$date = time();
+	echo "Inserting new version into the table";
+	$query = "Insert into version values ('date', 'verName', 'serverType','pending')";
+	$mysqli->query($query);	
+}
+
+function requestProcessor($request)
+{
+	echo "received request".PHP_EOL;
+	var_dump($request);
+	if(!isset($request['type']))
+	{
+		return "ERROR: unsupported message type";
+	}
+	switch ($request['type'])
+	{
+			case "versionCheck":
+				return versionCheck($request['serverType']);
+			case "newPackage":
+				return addVersion($request['serverType'],$request['versionNumber'],$request['packageName']);
+			
+			
+	}
+	return array("returnCode"=> '0', 'message'=>"Server received request and processed.");
+			
+}		
+
+$server = new rabbitMQServer("testRabbitMQ.ini","deploymentServer");
+echo "Deployment Server Begin".PHP_EOL;
+$server->process_requests('requestProcessor');
+echo "Deployment Server Ends".PHP_EOL;
+exit();
+?>		
