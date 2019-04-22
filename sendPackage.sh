4ae1608c530861e17fdbafbc80b5b8f14784567f
Adam <ak866@njit.edu>
2019-04-22 11:02:22 -0400

    Added tar functins to seperate other servers FE,BE,DMZ

--- sendPackage.sh
+++ sendPackage.sh
@@ -6,7 +6,13 @@ versionNum=$2
 echo "Creating package for "$serverType" version #: "$versionNum
 cd /home/$USER/git/
 
-tar -cf $serverType'_'$versionNum.tar --exclude-vcs IT490Spaghetti
+if [$serverType == "fe"]
+	tar -cf $serverType'_'$versionNum.tar --exclude-vcs IT490Spaghetti --exclude='IT490Spaghetti/backend' --exclude='IT490Spaghetti/DMZ'
+elif [$serverType == "be"]
+	tar -cf $serverType'_'$versionNum.tar --exclude-vcs IT490Spaghetti/backend
+elif [$serverType == "dmz"]
+	tar -cf $serverType'_'$versionNum.tar --exclude-vcs IT490Spaghetti/DMZ
+
 
 echo "Package "$serverType'_'$versionNum" was created"
 
