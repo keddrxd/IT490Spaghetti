6ea1b636e341bc9fe142a4799d1adbd563c7065c
Adam Kedra <47116358+keddrxd@users.noreply.github.com>
2019-04-23 18:45:14 -0400

    Add files via upload

---
+++ deployment/setPackageGood.php
@@ -0,0 +1,23 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+global $argv;
+
+
+function setPackageStatusGood($server)
+{
+	echo "Sending Request to change Package Status from Pending to Good.".PHP_EOl;
+	$client = new rabbitMQClient("testRabbitMQ.ini","deploymentServer");
+	$request = array();
+	$request['type'] = "setStatusGood"; 
+	$request['serverType'] = $server;
+	$client->send_request($request);
+	echo "Request has been sent successfully.".PHP_EOl;
+
+}
+$serverType = $argv[1];
+setPackageStatusGood($serverType);
+
+?>
