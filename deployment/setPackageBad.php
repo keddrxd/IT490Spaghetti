8476dba2468f1d2a7f88b2bf40e17deb3fa989c4
adam <ak866@njit.edu>
2019-04-23 20:04:29 -0400

    added bad

---
+++ deployment/setPackageBad.php
@@ -0,0 +1,23 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+global $argv;
+
+
+function setPackageStatusBad($server)
+{
+	echo "Sending Request to change Package Status from Pending to Bad.".PHP_EOL;
+	$client = new rabbitMQClient("testRabbitMQ.ini","deploymentServer");
+	$request = array();
+	$request['type'] = "setStatusBad"; 
+	$request['serverType'] = $server;
+	$client->send_request($request);
+	echo "Request has been sent successfully.".PHP_EOL;
+
+}
+$serverType = $argv[1];
+setPackageStatusGood($serverType);
+
+?>
