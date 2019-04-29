f415596c50de58404349b0a1ee49a105fd3934b2
andrew8197 <47332089+andrew8197@users.noreply.github.com>
2019-04-29 17:21:32 -0400

    Update mainPage.php

--- mainPage.php
+++ mainPage.php
@@ -254,12 +254,18 @@ margin-right: 75px;
 		</center>
 		</div>
 	
-	<?php  
-$friendName = $_POST['username']; 
+<?php  
 echo "<table border=1 cellspacing=0 cellpading=0>  
 <tr> <td><font color=blue>Friends List</td> </tr>    
-<tr> <td><font color=blue>$friendName</td> </tr>  
+<tr> <td><font color=blue>$friendName</td> </tr>
 </table>";  
+for($i = 0 ; $i < 10 ; $i++)
+{
+	if($_SESSION['friendsList'][$i] == $_POST['username'])
+	{
+		$friendName = $_SESSION['friendsList'][$i];
+	}
+}
 ?>  
 	
 	<form action = "mainPage.php" method="POST">
