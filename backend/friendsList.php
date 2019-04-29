2c7f6ef995c3daa406d4be53999bd66202b0751e
andrew8197 <47332089+andrew8197@users.noreply.github.com>
2019-04-29 19:26:07 -0400

    Create friendsList.php

---
+++ backend/friendsList.php
@@ -0,0 +1,15 @@
+<?php
+require ('rabbitFunc.php');
+$username = $_SESSION['username'];
+$response = friends($username);
+if($response != false)
+{
+    $sessionData = json_decode($response, true);
+    $_SESSION['friendList'] = array();
+    $num = 5;
+    for($i = 0 ; $i < $num ; $i++)
+    {
+      $_SESSION['friendsList'][$i] = $sessionData[$i];
+    }
+}
+?>
