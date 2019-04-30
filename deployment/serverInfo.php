72b9733bdc8f69a3e19c70bad51da21e1acbe311
Adam Kedra <47116358+keddrxd@users.noreply.github.com>
2019-04-30 16:54:39 -0400

    Add files via upload

---
+++ deployment/serverInfo.php
@@ -0,0 +1,35 @@
+#!/usr/bin/php
+<?php
+global $argv;
+
+$serverType = $argv[1];
+$packageVersion = $argv[2];
+$tierLevel = $argv[3];
+
+$host = 'localhost';
+$user = 'adam';
+$pass = 'deployPass';
+$db = 'versionControl';
+
+mysqli = new mysqli($host,$user,$pass,$db);
+if ($mysqli->connect_error)
+{
+	die('Connection Error.');
+}
+
+if ($tierLevel == "ipAddress"
+{
+	$query = "select * from machineIP where serverName = '$serverType' and tierType = '$tierLevel'";
+	$package = $mysqli->query($query);
+	$package = $package->fetch_assoc();
+	echo $package['ipAddress'];
+}
+if ($tierLevel == "serverName"
+{
+	$query = "select * from machineIP where serverName = '$serverType' and tierType = '$tierLevel'";
+	$package = $mysqli->query($query);
+	$package = $package->fetch_assoc();
+	echo $package['serverName'];
+}
+
+?>
---
+++ deployment/versionInfo.php
@@ -0,0 +1,47 @@
+#!/usr/bin/php
+
+<?php
+global $argv;
+
+$serverType = $argv[1];
+$packageVersion = $argv[2];
+$tierLevel = $argv[3];
+
+$host = 'localhost';
+$user = 'adam';
+$pass = 'deployPass';
+$db = 'versionControl';
+
+mysqli = new mysqli($host,$user,$pass,$db);
+if ($mysqli->connect_error)
+{
+	die('Connection Error.');
+}
+
+if ($package == "newest")
+{
+	$query = "select verName from version ='$serverType' and status = 'pending' order by verName";
+	$package = $mysqli->query($query);
+	$package = $package->fetch_assoc();
+	echo $package['package'];
+}
+elseif ($package == "good")
+{
+	$query = "select verName from version ='$serverType' and status = 'good' order by verName desc limit 1 ";
+	$package = $mysqli->query($query);
+	$package = $package->fetch_assoc();
+	echo $package['package'];
+}
+elseif ($package == "rollback")
+{
+	
+	$query = "select verName from version ='$serverType' and status = 'pending' order by verName";
+	$package = $mysqli->query($query);
+	$package = $package->fetch_assoc();
+	echo $package['package'];
+}
+else
+{
+	echo "$package";
+}
+?>
