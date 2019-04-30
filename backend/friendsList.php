f800acf785e37435bdb09e18cf04c1cd7a754ef2
andrew8197 <47332089+andrew8197@users.noreply.github.com>
2019-04-30 18:59:56 -0400

    Update friendsList.php

--- backend/friendsList.php
+++ backend/friendsList.php
@@ -8,7 +8,7 @@ if($response != false)
 {
     $sessionData = json_decode($response, true);
     $_SESSION['friendList'] = array();
-    $num = 5;
+    $num = 4;
     for($i = 0 ; $i < $num ; $i++)
     {
       $_SESSION['friendsList'][$i] = $sessionData[$i];
