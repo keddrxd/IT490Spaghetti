9ee01107b1a6e4c1a86465b69201f4c8f359b216
andrew8197 <47332089+andrew8197@users.noreply.github.com>
2019-05-02 20:49:08 -0400

    Update mainPage.php

--- mainPage.php
+++ mainPage.php
@@ -157,7 +157,7 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST')
 			$friend[$i] = $_SESSION['friendsList'][$i];
 			
 		}
-		echo $friend[1];
+		echo $friend;
 		//echo "<br>".$_SESSION['friendsList'][1];
 		/*for($i = 0 ; $i < 10 ; $i++)
 		{
