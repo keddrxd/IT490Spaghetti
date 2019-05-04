3c8808e25f03d43d65b7fe2da12de83c846ecbbe
andrew8197 <47332089+andrew8197@users.noreply.github.com>
2019-05-03 20:03:52 -0400

    Update mainPage.php

--- mainPage.php
+++ mainPage.php
@@ -150,7 +150,7 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST')
 	if(isset($_POST['friend']))
 	{
 		require 'backend/friendsList.php'; #change file name
-		echo "HELLO THIS IS A TEST";
+		echo "HELLO THIS IS A TEST".$_SESSION['friendsList'][0];
 		$num = 4;
 		$friend = array();
 		for($i = 0 ; $i < $num ; $i++)
