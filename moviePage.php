77e5571089dadd138c589eb176bdabae9a599966
andrew8197 <47332089+andrew8197@users.noreply.github.com>
2019-05-09 15:47:37 -0400

    Update moviePage.php

--- moviePage.php
+++ moviePage.php
@@ -148,7 +148,7 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST')
           </div>
         </div>
       </div>
-	<center><button name = "first">Submit</button></center>
+	<center><button class="btn btn-lg btn-secondary btn-block" name = "first" href="#">Submit</button></center>
 	      <!--<a class="btn btn-lg btn-secondary btn-block" name = "first" href="#">Submit</a>-->
     </div>
     <!-- /.row -->
