831acbacd765eed9b978efcbef50ed9889bf7ad8
andrew8197 <47332089+andrew8197@users.noreply.github.com>
2019-05-09 13:43:29 -0400

    Update mainPage.php

--- mainPage.php
+++ mainPage.php
@@ -100,7 +100,7 @@
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item dropdown">
-            <a class="nav-link " href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a class="nav-link " href="#" id="navbarDropdownBlog" data-toggle="dropdown" name = "logout" aria-haspopup="true" aria-expanded="false">
               Log Out
             </a>
             
