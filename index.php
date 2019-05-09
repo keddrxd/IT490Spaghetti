77a7ffc1b7caad8dc6a0c9993ac65ca54b99610c
Aalap Parekh <ap732@njit.edu>
2019-05-09 12:46:30 -0400

    homepage bootstrap

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-</head>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Rotten Spaghetti</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/modern-business.css" rel="stylesheet">
+
+</head>
 <?php
 session_start();
 if( $_SERVER['REQUEST_METHOD'] == 'POST')
@@ -19,46 +33,144 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST')
 	}
 }
 ?>
-<body background="spaghetti.jpg">
-<form action = "index.php" method = "POST">
-<head>
-<center>
-<h1> Welcome to Rotten Spaghetti </h1>
-<style>
-div {
-    background-color: red;
-    width: 300px;
-    border: 5px solid black;	    
-    padding: 25px;
-    margin: 25px;
-}
-</style>
-</head>
-<center>
-		<div>
-		<h2> Returning User? Login </h2> 
-		<center>Enter Username <input type= "text" required name="username"/><br><br>
-		Enter Password <input type="password" required name="password" /><br><br>
-		<button name = "login"> Login </button> 
-		</center>
-		</div>
-		</form>
-<form action = "index.php" method = "POST">
-<center>
-		<div>
-		<h2> New User? Sign up </h2>
-		<center>Enter First Name <input type= "text" required name="firstName"/><br><br>
-		Enter Last Name <input type="text" required name="lastName"  /><br><br>
-		Enter Username <input type="text" required name="username" /><br><br>
-		Enter Email <input type="email" required name="email" /><br><br>
-		Enter Password <input type="password" required name="password" /><br><br>
-		<button name = "register"> Register </button>
-		</center>
-		
-		</div>
-			
-</center>
+<body>
 
-</form>
+  <!-- Navigation -->
+  <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="index.html">Rotten Spaghetti</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+         
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <header>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      
+      <div class="carousel-inner" role="listbox">
+        <!-- Slide One - Set the background image for this slide in the line below -->
+        <div class="carousel-item active" style="background-image: url(spaghetti.jpg)">
+          <div class="carousel-caption d-none d-md-block">
+            <h3>Rotten Spaghetti</h3>
+            <p>The best movie recommender out there!</p>
+          </div>
+        
+        </div>
+      </div>
+     
+    </div>
+  </header>
+
+  
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-10 col-xl-9 mx-auto">
+        <div class="card card-signin flex-row my-5">
+          <div class="card-img-left d-none d-md-flex">
+             <!-- Background image for card set in CSS! -->
+          </div>
+
+          <div class="card-body">
+            <h5 class="card-title text-center">Returning User? Sign in</h5>
+            <form class="form-signin">
+              <div class="form-label-group">
+				<form action = "index.php" method = "POST">
+
+                <input type="text" required name="username" class="form-control" placeholder="Username" required autofocus> <br>
+              
+              </div>
+
+             
+
+              <div class="form-label-group">
+                <input type="password" required name="password" class="form-control" placeholder="Password" required> <br>
+                
+              </div>
+              
+              
+
+              <button class="btn btn-lg btn-primary btn-block text-uppercase" name = "login" type="submit">Sign in</button>
+             
+              <hr class="my-4">
+             
+            </form>
+			</form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+    <div class="container">
+    <div class="row">
+      <div class="col-lg-10 col-xl-9 mx-auto">
+        <div class="card card-signin flex-row my-5">
+          <div class="card-img-left d-none d-md-flex">
+             <!-- Background image for card set in CSS! -->
+          </div>
+          <div class="card-body">
+            <h5 class="card-title text-center">New User? Sign up</h5>
+            <form class="form-signin">
+              <div class="form-label-group">
+			  	<form action = "index.php" method = "POST">
+
+                <input type="text" required name="firstName" class="form-control" placeholder="First Name" required autofocus>
+                <br>
+              </div>
+				<div class="form-label-group">
+                <input type="text" required name="lastName" class="form-control" placeholder="Last Name" required autofocus>
+            <br>
+              </div>
+              <div class="form-label-group">
+                <input type="email" required name="email" class="form-control" placeholder="Email address" required>
+               <br>
+              </div>
+              
+              
+
+              <div class="form-label-group">
+                <input type="password" required name="username" class="form-control" placeholder="Username" required>
+                <br>
+              </div>
+              
+              <div class="form-label-group">
+                <input type="password" required name="password" class="form-control" placeholder="Password" required>
+               <br>
+              </div>
+
+              <button class="btn btn-lg btn-primary btn-block text-uppercase" name = "register" type="submit">Register</button>
+              
+              <hr class="my-4">
+             
+            </form>
+			</form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Rotten Spaghetti</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
 </body>
+
 </html>
+
