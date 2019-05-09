18efeb50998253ab4a03efc093fc37191f02849e
Aalap Parekh <ap732@njit.edu>
2019-05-09 12:48:37 -0400

    page 3 bootstrap

--- moviePage.php
+++ moviePage.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
 <?php
 session_start();
@@ -21,162 +22,166 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST')
 	}
 }
 ?>
-<title>First Timers</title>
 
-<style>
-		input[type = 'checkbox']
-		{
-		-webkid-appearance: none;
-		width: 30px;
-		height:30px;
-		background: white;
-		border-radius:5px;
-		border:2px solid #555;
-		
-		}
-	div.username
-{
-padding: 1%;
-color:white;
-background-color: light gray;
-height: 10px;
-width: 15px;
-border: white;
-}	
-         div.comedy
-		 { 
-         padding : 1%; 
-         color: white; 
-		 float: left;
-         background-color: light gray; 
-		 height: 400px;
-         width: 300px; 
-         border: white; 
-         } 
-		 div.animation
-		 { 
-         padding : 1%; 
-         color: white; 
-		 float: left;
-         background-color: light gray; 
-		 height: 400px;
-         width: 300px; 
-         border: white; 
-         } 
-		 div.horror
-		 { 
-         padding : 1%; 
-         color: white; 
-		 float: left;
-         background-color: light gray; 
-		 height: 400px;
-         width: 300px; 
-         border: white; 
-         } 
-		 div.action
-		 { 
-         padding : 1%; 
-         color: white; 
-		 float: left;
-         background-color: light gray; 
-		 height: 400px;
-         width: 300px; 
-         border: white; 
-         } 
-		 div.sci-fi
-		 { 
-         padding : 1%; 
-         color: white; 
-		 float: left;
-         background-color: light gray; 
-		 height: 400px;
-         width: 300px; 
-         border: white; 
-         } 
-		 div.romance
-		 { 
-         padding : 1%; 
-         color: white; 
-		 float: left;
-         background-color: light gray; 
-		 height: 400px;
-         width: 300px; 
-         border: white; 
-         } 
-		 #submit {
-		<!-- margin-left: 1250px;-->
-		 <!--margin-top: -85px;s4e1s-->
-		 float: auto:;
-		 width: 84px;
-		 height: 40px;   
-		 font-size:14px;
-		 font-weight:700;
-		 background-color: clear;
-}
-		 
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-</style>
+  <title>Rotten Spaghetti</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/modern-business.css" rel="stylesheet">
+
 </head>
 
-<body background = "spaghetti.jpg">
-<form action = "moviePage.php" method = "POST">
-<center>
-<h1 style="font-size:60px;" >Welcome!</h1>
-<!--<center>
-<div class = "username">
-Please type your username one more time: <input type = "text" name = "username"/>
-</center>-->
-<h2 style = "font-size: 35px; ">Tell us what you may like </h2>
-</center>
-<center>
-<div class="comedy">
-<img src="dumber.jpg" alt="dumber" style="width:250px;height:350px;">
-<br><input type="checkbox" name="comedy" value="comedy"><br>
-</center>
-</div>
-<center>
-<div class="animation">
-<img src="nemo.jpg" alt="nemo" style="width:250px;height:350px;">
-<br><input type="checkbox" name="animation" value="animation"><br>
-</div>
-</center>
-<center>
-<div class="horror">
-<img src="insidious.jpg" alt="insidious" style="width:250px;height:350px;">
-<br><input type="checkbox" name="horror" value="horror"><br>
-</div>
-</center>
-<center>
-<div class="action">
-<img src="avengers.jpg" alt="avengers" style="width:250px;height:350px;">
-<br><input type="checkbox" name="action" value="action"><br>
-</div>
-</center>
-<center>
-<div class="sci-fi">
-<img src="starwars.jpg" alt="starwars" style="width:250px;height:350px;">
-<br><input type="checkbox" name="sci-fi" value="sci-fi"><br>
-</div>
-</center>
+<body>
+ <!-- Navigation -->
+  <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="index.html">Rotten Spaghetti</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+         
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  
 
-<center>
-<div class="romance">
-<img src="titanic.jpg" alt="titanic" style="width:250px;height:350px;">
-<br><input type="checkbox" name="romance" value="romance"><br>
-</div>
-</center>
+  <header>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <!-- Slide One - Set the background image for this slide in the line below -->
+        <div class="carousel-item active" style="background-image: url(spaghetti.jpg)">
+          <div class="carousel-caption d-none d-md-block">
+            <h3>Welcome to Rotten Spaghetti</h3>
+            <p>The best movie recommender out there!</p>
+          </div>
+        
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </header>
 
+  <!-- Page Content -->
+  <div class="container">
 
-<div id = "submit">
-		<button name = "first" style = "height:50px;width:80px"><font size = 4> Submit </font></button>
-		</div>
-<center>
+    <h1 class="my-4">Please tell us what you may like:</h1>
 
+    
+    <!-- Portfolio Section -->
 
+    <div class="row">
+      <div class="col-lg-4 col-sm-6 portfolio-item">
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="https://m.media-amazon.com/images/M/MV5BZDQwMjNiMTQtY2UwYy00NjhiLTk0ZWEtZWM5ZWMzNGFjNTVkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg" alt=""></a>
+          <br><center><input type="checkbox" name="comedy" value="comedy"></center><br>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6 portfolio-item">
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SY1000_CR0,0,675,1000_AL_.jpg" alt=""></a>
+          <br><center><input type="checkbox" name="action" value="action"></center><br>
+		 <div class="card-body">
+            
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6 portfolio-item">
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="https://m.media-amazon.com/images/M/MV5BZjMxYzBiNjUtZDliNC00MDAyLTg3N2QtOWNjNmNhZGQzNDg5XkEyXkFqcGdeQXVyNjE2MjQwNjc@._V1_.jpg" alt=""></a>
+          <br><center><input type="checkbox" name="animation" value="animation"></center><br>
+		  <div class="card-body">
+            
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6 portfolio-item">
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="https://m.media-amazon.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SY999_SX666_AL_.jpg" alt=""></a>
+          <br><center><input type="checkbox" name="sci-fi" value="sci-fi"></center><br>
+		  <div class="card-body">
+         
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6 portfolio-item">
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="https://m.media-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SY1000_CR0,0,671,1000_AL_.jpg" alt=""></a>
+          <br><center><input type="checkbox" name="romance" value="romance"></center><br>
+		  <div class="card-body">
+            <h4 class="card-title">
+              
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6 portfolio-item">
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="https://m.media-amazon.com/images/M/MV5BMTYyOTAxMDA0OF5BMl5BanBnXkFtZTcwNzgwNTc1NA@@._V1_SY1000_CR0,0,675,1000_AL_.jpg" alt=""></a>
+          <br><center><input type="checkbox" name="horror" value="horror"></center><br>
+		  <div class="card-body">
+            <h4 class="card-title">
+             
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /.row -->
 
+    
 
-</center>
-</center>
+    <hr>
+
+    <!-- Call to Action Section -->
+    <div class="row mb-4">
+      <div class="col-md-8">
+        
+      </div>
+      <div class="col-md-4">
+		
+        <a class="btn btn-lg btn-secondary btn-block" name = "first" href="#">Submit</a>
+		
+      </div>
+    </div>
+
+  </div>
+
+  <!-- /.container -->
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Rotten Spaghetti</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 </body>
+
 </html>
